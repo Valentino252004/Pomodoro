dead99447f2ac11d6715a9566f8adcb810230587
Patey Valentin <valentin.patey@etu.unicaen.fr>
2023-09-16 13:57:14 +0200

    Correction setInterval Bug / better Time Display

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,17 +1,21 @@
 //initialisation of variables
-let work = 2;
+let workTime = 15;
 let breakTime = 1;
 let seconds = 0;
 let working = true;
 
 //Initialisation of the HTML page
-document.getElementById("timer").innerHTML = work + ":00"
+updateHTMLTimer("work");
 
 //Listeners for the start and reset buttons
 let start = document.getElementById("start");
 start.addEventListener("click", () => {
-    setInterval(timer(), 1000);
-    console.log("1");
+    if (working) {
+     var timerInterval = setInterval(workTimer, 1000);
+    }
+    else {
+        //TO DO
+    }
 })
 
 let reset = document.getElementById("reset");
@@ -19,25 +23,60 @@ reset.addEventListener("click", () => {
     alert("reset")
 })
 
-setInterval( timer(), (1000));
-
-function timer() {
-    console.log("3")
+function workTimer() {
     if (seconds == 0) {
+        if (workTime==0) {
+            clearInterval(timerInterval);
+        }
+        workTime-=1;
         seconds = 59;
-        if (working) {
-            work-=1;
-        }
-        else {
-            breakTime -= 1;
-        }
     }
     else {
         seconds-=1;
     }
-    updateHTMLTimer();
+    updateHTMLTimer("work");
 }
 
-function updateHTMLTimer() {
-    document.getElementById("timer").innerHTML = work + ":" + seconds;
+//TO DO 
+function breakTimer() {
+    if (seconds == 0) {
+        if (workTime==0) {
+            clearInterval(timerInterval);
+        }
+        workTime-=1;
+        seconds = 59;
+    }
+    else {
+        seconds-=1;
+    }
+    updateHTMLTimer("break");
+}
+
+
+function updateHTMLTimer(statut) {
+    let time = "";
+
+    if (statut=="work") {
+        if (workTime < 10) {
+            time += "0";
+        }
+        time += workTime + ":";
+        if (seconds<10) {
+            time += "0";
+        }
+        time += seconds;
+    }
+
+    else if (statut == "break") {
+        if (breakTime < 10) {
+            time += "0";
+        }
+        time += breakTime + ":";
+        if (seconds<10) {
+        time += "0";
+        }
+        time += seconds;
+    }
+
+    document.getElementById("timer").innerHTML = time;
 }
