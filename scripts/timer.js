489a026010e6fdeb60568e5e882eaf5ee97ed6ec
Patey Valentin <valentin.patey@etu.unicaen.fr>
2023-09-16 14:52:48 +0200

    functional js code

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,20 +1,26 @@
 //initialisation of variables
-let workTime = 15;
-let breakTime = 1;
+let workvar = 1;
+let workTime = workvar;
+
+let breakvar = 2;
+let breakTime = breakvar;
+
 let seconds = 0;
 let working = true;
 
+let workTimerInterval;
+let breakTimerInterval;
 //Initialisation of the HTML page
-updateHTMLTimer("work");
+updateHTMLTimer();
 
 //Listeners for the start and reset buttons
 let start = document.getElementById("start");
 start.addEventListener("click", () => {
     if (working) {
-     var timerInterval = setInterval(workTimer, 1000);
+        workTimerInterval = setInterval(workTimer, 10);
     }
     else {
-        //TO DO
+        breakTimerInterval = setInterval(breakTimer, 10);
     }
 })
 
@@ -23,40 +29,62 @@ reset.addEventListener("click", () => {
     alert("reset")
 })
 
+
 function workTimer() {
     if (seconds == 0) {
         if (workTime==0) {
-            clearInterval(timerInterval);
+            swapTimer();
+            clearInterval(workTimerInterval);
         }
-        workTime-=1;
-        seconds = 59;
+        else {
+            workTime-=1;
+            seconds = 59;
+        }
     }
     else {
         seconds-=1;
     }
-    updateHTMLTimer("work");
+    updateHTMLTimer();
 }
 
-//TO DO 
 function breakTimer() {
     if (seconds == 0) {
-        if (workTime==0) {
-            clearInterval(timerInterval);
+        if (breakTime==0) {
+            swapTimer();
+            clearInterval(breakTimerInterval);
         }
-        workTime-=1;
-        seconds = 59;
+        else{
+            breakTime-=1;
+            seconds = 59;
+        }   
     }
     else {
         seconds-=1;
     }
-    updateHTMLTimer("break");
+    updateHTMLTimer();
 }
 
 
-function updateHTMLTimer(statut) {
+function swapTimer() {
+    if (working) {
+        working = false;
+        workTime = workvar;
+        console.log("work : " + workTime);
+        updateHTMLTimer();
+    }
+    else {
+        working=true;
+        breakTime = breakvar;
+        console.log("break : " + breakTime);
+        updateHTMLTimer();
+    }
+}
+
+
+function updateHTMLTimer() {
     let time = "";
 
-    if (statut=="work") {
+    if (working) {
         if (workTime < 10) {
             time += "0";
         }
@@ -67,7 +95,7 @@ function updateHTMLTimer(statut) {
         time += seconds;
     }
 
-    else if (statut == "break") {
+    else {
         if (breakTime < 10) {
             time += "0";
         }
